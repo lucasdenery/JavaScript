1f3022f6a86dbd9a344b856a15e340614c3990dd
Lucas Nery <141687556+lucasdenery@users.noreply.github.com>
2025-05-21 16:45:33 -0300

    nova aula

---
+++ aula012/ex012.js
@@ -0,0 +1,10 @@
+var horario = "noite" 
+console.log(`Seu horaio ainda é ${horario}.`)
+if (horario == "dia"){
+    console.log('Bom dia!')
+} else if (horario == "tarde"){
+    console.log('Boa tarde!')
+}
+else{
+    console.log('Boa noite!')
+}
